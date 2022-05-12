5ad7efb9e4797559b7be2195dafc365035693743
micaelsn <micaelnascimentocc@gmail.com>
2022-05-12 17:01:04 -0300

    Initial Commit

---
+++ iChat/iChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  iChatApp.swift
+//  iChat
+//
+//  Created by micael nascimento on 12/05/22.
+//
+
+import SwiftUI
+
+@main
+struct iChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
